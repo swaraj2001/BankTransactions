d9e5b961bff3eb7f8753a77ca0f751764a84a684
swaraj2001 <69841511+swaraj2001@users.noreply.github.com>
2024-01-22 22:31:06 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,1 @@
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Number : ");
-        int number = sc.nextInt();
-
-        System.out.println("Enter Your Name :");
-        sc.nextLine();
-        String name = sc.nextLine();
-
-        System.out.println(number + " " +name);
-    }
-}
